6355e2b005e9f70117996755b5bdffc3a6ef4690
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-02-01 16:16:27 -0400

    Remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """The setup script."""
 
 from setuptools import find_packages, setup
@@ -10,16 +7,6 @@ with open('README.rst') as readme_file:
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
-
-requirements = []
-
-setup_requirements = ['pytest-runner']
-
-test_requirements = ['pytest']
-
-with open('requirements_dev.txt', 'r') as f:
-    dev_requirements = [l for l in f.read().split('\n') if l.strip()]
-dev_requirements += requirements
 
 setup(
     author="Ivan Ogasawara",
@@ -35,17 +22,13 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     description="PoC Dependabot",
-    install_requires=requirements,
-    extras_require={'dev': dev_requirements},
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='poc_dependabot',
     name='poc_dependabot',
     packages=find_packages(include=['poc_dependabot']),
-    setup_requires=setup_requirements,
     test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/xmnlab/poc_dependabot',
     version='1.0.0',
     zip_safe=False,
